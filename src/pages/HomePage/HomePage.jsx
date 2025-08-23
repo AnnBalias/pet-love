94829ce69a4cae08b7cd1437a3460fbf3a0726a5
AnnBalias <annbalias@gmail.com>
2025-08-23 18:48:02 +0300

    add-HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
+import css from './HomePage.module.css';
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className={css.homePageContainer}>
+      <div className={css.homePageInfo}>
+        <h1 className={css.homePageTitle}>
+          Take good <span className={css.homePageTitleSpan}>care</span> of your
+          small pets
+        </h1>
+        <p className={css.homePageText}>
+          Choosing a pet for your home is a choice that is meant to enrich your
+          life with immeasurable joy and tenderness.
+        </p>
+      </div>
+      <div className={css.homePageImg}></div>
     </div>
   );
 }
